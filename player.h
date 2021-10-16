05956c825a4c48dcf156b64da962d2f463b6ff1f
Alexander Chebotin <61747868+axothy@users.noreply.github.com>
2021-10-16 20:53:12 +0300

    Add files via upload

---
+++ player.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "playboard.h"
+
+class Playboard;
+enum CheckerColor;
+enum CheckerHierarchy;
+
+class Player {
+protected:
+	Playboard* gameCheckers;
+public:
+	Player(Playboard* instance) { gameCheckers = instance; }
+
+	virtual bool Turn(int x, int y, CheckerColor b_or_w);
+	
+};
+
+class BotV1 : public Player {
+
+public:
+	BotV1(Playboard* instance) : Player(instance) {}
+	bool Turn(int x, int y, CheckerColor b_or_w);
+};
+
+class BotV2 : public Player {
+
+public:
+	BotV2(Playboard* instance) : Player(instance) {}
+	bool Turn(int x, int y, CheckerColor b_or_w);
+};
